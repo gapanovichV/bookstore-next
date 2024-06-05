7172286a1f0314c00d56933da655fa70ac8cbcd9
Vlad Gapanovich <vladgapanovich172@gmail.com>
2024-06-05 23:49:39 +0300

    feat: isLogined Header

--- components/modules/Header/Header.tsx
+++ components/modules/Header/Header.tsx
@@ -2,15 +2,17 @@
 
 import clsx from "clsx"
 import Link from "next/link"
-import {useRouter } from "next/navigation"
+import { useRouter } from "next/navigation"
 
 import { Button } from "@/components/elements/Button/Button"
 import { SearchResults } from "@/components/elements/SearchResults/SearchResults"
 import { RouteEnum } from "@/types/route.type"
 
 import styles from "./Header.module.scss"
+import { useState } from "react"
 
 const Header = () => {
+  const [isLogin, setIsLogin] = useState(false)
   const router = useRouter()
   return (
     <header className={clsx(styles.header)}>
@@ -19,8 +21,26 @@ const Header = () => {
           <img src="/img/Logo.svg" alt="Logo" />
         </Link>
         <SearchResults />
-        <Button size="small" variant="ghost" onClick={() => router.push(RouteEnum.CATALOG)}>Discover</Button>
-        <Button size="medium" variant="fill" onClick={() => router.push(RouteEnum.SIGN_IN)}>Login</Button>
+        <Button size="small" variant="ghost" onClick={() => router.push(RouteEnum.CATALOG)}>
+          Discover
+        </Button>
+        {!isLogin ? (
+          <>
+            <Button size="medium" variant="fill" onClick={() => router.push(RouteEnum.SIGN_IN)}>
+              Login
+            </Button>
+          </>
+        ) : (
+          <div className={clsx(styles.header__logined)}>
+            <img src="/img/Notification.svg" alt="Notification icon" />
+            <Link href={RouteEnum.CART}>
+              <img src="/img/Cart.svg" alt="Cart Icon" />
+            </Link>
+            <Link href={RouteEnum.PROFILE}>
+              <img src="/img/ProfileTEST.png" alt="Profile picture" />
+            </Link>
+          </div>
+        )}
       </div>
     </header>
   )
