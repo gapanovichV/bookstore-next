2fb9d9adb10cd07f75615d34afc6bce6ff8972d1
Vlad Gapanovich <vladgapanovich172@gmail.com>
2024-06-24 00:04:08 +0300

    feat: add prop line tab

--- components/elements/Tab/Tab.tsx
+++ components/elements/Tab/Tab.tsx
@@ -8,17 +8,29 @@ type TabPosition = "horizontal" | "vertical"
 interface TabProps {
   className?: string
   position: TabPosition
+  visibleLine?: boolean
   tabs: { label: string }[]
   currentItem: { label: string }
   changeItem: Dispatch<SetStateAction<number>>
 }
-export const Tab = ({ position, className, tabs, currentItem, changeItem }: TabProps) => {
+export const Tab = ({
+  tabs,
+  position,
+  visibleLine = true,
+  currentItem,
+  changeItem,
+  className
+}: TabProps) => {
   return (
     <div className={clsx(styles.tab, styles[position], className)}>
       {tabs.map((tab, index) => (
         <div
           key={index}
-          className={clsx(styles.tab__label, { [styles.active]: currentItem.label === tab.label })}
+          className={clsx(
+            styles.tab__label,
+            { [styles.active]: currentItem.label === tab.label },
+            { [styles.line]: !visibleLine }
+          )}
           onClick={() => changeItem(index)}
         >
           {tab.label}
